955ad7e768cb408e17b208aa413f0e6fcae82e55
Maker By Night <2565361+mshafer1@users.noreply.github.com>
2026-03-14 20:21:29 -0500

    add active tag to distinct filter

--- quick-filter-table/src/components/distinctFilter.vue
+++ quick-filter-table/src/components/distinctFilter.vue
@@ -1,5 +1,6 @@
 <template>
-    <FilterIcon v-if="filterable" @clicked="showFilter = !showFilter; console.log('clicked on', header)"></FilterIcon>
+    <FilterIcon v-if="filterable" @clicked="showFilter = !showFilter; console.log('clicked on', header)"
+        :active="active"></FilterIcon>
     {{ header.text }}
     <div v-if="filterable && showFilter" class="filter-dropdown" @click.stop>
         <select class="form-control form-control-sm form-select" v-model="value" @change="update_filter">
@@ -27,10 +28,12 @@ export default {
             showFilter: false,
             uniqueValues: sortedUniq(this.all_values.sort()),
             value: null,
+            active: false,
         }
     },
     methods: {
         update_filter() {
+            this.active = this.value != null && this.value.length > 0;
             this.$emit('update-filter', this.value);
         }
     }
